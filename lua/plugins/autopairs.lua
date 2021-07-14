bd4a8390f2aa79feb27efe91447179dee888dbc4
Khoa Nguyen <khoa.nguyen@employmenthero.com>
2021-07-14 09:40:45 +0700

    endwise settings

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,31 +1,12 @@
 local npairs = require('nvim-autopairs')
-local endwise = require('nvim-autopairs.ts-rule').endwise
 
 npairs.setup{
   ignored_next_char = "[%w%:%.]"
 }
 
-npairs.add_rules({
-  endwise('%sdo$',              'end', 'ruby', nil),
-  endwise('%sdo%s|.*|$',        'end', 'ruby', nil),
-  endwise('begin$',             'end', 'ruby', nil),
-  endwise('def%s.+$',           'end', 'ruby', nil),
-  endwise('module%s.+$',        'end', 'ruby', nil),
-  endwise('class%s.+$',         'end', 'ruby', nil),
-  endwise('[%s=]%sif%s.+$',     'end', 'ruby', nil),
-  endwise('[%s=]%sunless%s.+$', 'end', 'ruby', nil),
-  endwise('[%s=]%scase%s.+$',   'end', 'ruby', nil),
-  endwise('[%s=]%swhile%s.+$',  'end', 'ruby', nil),
-  endwise('[%s=]%suntil%s.+$',  'end', 'ruby', nil),
+npairs.add_rules(require('nvim-autopairs.rules.endwise-ruby'))
+
+require('nvim-autopairs.completion.compe').setup({
+  map_cr = true,
+  map_complete = true,
 })
-
-_G.completion_confirm=function()
-  if vim.fn.pumvisible() == 0 or ((vim.call('complete_info', {'selected' }).selected or -1) == -1)
-  then
-    return npairs.autopairs_cr()
-  else
-    return vim.fn["compe#confirm"]('<cr>')
-  end
-end
-
-map('i' , '<CR>','v:lua.completion_confirm()', { silent = true, expr = true })
