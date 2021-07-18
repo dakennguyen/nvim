4a5348f700a5d132a98330ec0455237fa571e0c1
Khoa Nguyen <khoa.nguyen@employmenthero.com>
2021-07-18 14:58:06 +0700

    git time machine

--- lua/plugins/vim-fugitive.lua
+++ lua/plugins/vim-fugitive.lua
@@ -10,5 +10,6 @@ vim.cmd[[ command! -nargs=? Browse lua _G.open_in_browser(<q-args>) ]]
 
 map('n', 'gb', ':Git blame<CR>',               { silent = true }) -- Git Blame
 map('n', '<leader>gd', ':Gdiffsplit<CR>',      { silent = true }) -- Git diff
+map('n', '<leader>gt', ':0Gclog<CR>',          { silent = true }) -- Git time machine
 map('n', '<leader>gl', ':GBrowse<CR>',         { silent = true }) -- Git Browse
 map('v', '<leader>gl', [[:'<,'>.GBrowse<CR>]], { silent = true }) -- Git Browse in Visual mode
