5e22a1a18707dcbe707a98ffcdb25f17d5b974c0
Khoa Nguyen <khoa.nguyen@employmenthero.com>
2021-07-30 11:26:50 +0700

    ale setting

--- lua/plugins/vim-ale.lua
+++ lua/plugins/vim-ale.lua
@@ -1,16 +1,21 @@
+vim.g.ale_sign_error = '✘'
+vim.g.ale_sign_warning = ""
+
 vim.g.ale_linters_explicit = 1
 vim.g.ale_lint_on_text_changed = 'never'
 vim.g.ale_lint_on_insert_leave = 0
 vim.g.ale_lint_on_enter = 0
 vim.g.ale_fix_on_save = 1
 
-vim.g.ale_set_loclist = 0
-vim.g.ale_set_quickfix = 1
+vim.g.ale_set_loclist = 1
+vim.g.ale_set_quickfix = 0
 vim.g.ale_open_list = 0
 
 vim.g.ale_linters = {
   javascript = {'eslint', 'prettier'},
+  typescriptreact = {'eslint'},
 }
 vim.g.ale_fixers = {
   javascript = {'prettier'},
+  typescriptreact = {'eslint'},
 }
--- lua/theme.lua
+++ lua/theme.lua
@@ -4,3 +4,9 @@ vim.cmd([[colorscheme nord]])
 vim.cmd [[autocmd ColorScheme * hi Folded guifg=#4C566A guibg=NONE gui=italic]]
 vim.cmd [[autocmd ColorScheme * hi IncSearch guifg=white guibg=NONE]]
 vim.cmd [[autocmd ColorScheme * hi Search guifg=white guibg=NONE gui=underline,bold]]
+
+-- ALE
+vim.cmd [[autocmd ColorScheme * hi ALEWarning guifg=#EBCB8B]]
+vim.cmd [[autocmd ColorScheme * hi ALEError guifg=#BF616A]]
+vim.cmd [[autocmd ColorScheme * hi ALEWarningSign guifg=#EBCB8B]]
+vim.cmd [[autocmd ColorScheme * hi ALEErrorSign guifg=#BF616A]]
