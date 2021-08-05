6b3f53726adf5ad75010e72b95d07e29fd2bfdce
Khoa Nguyen <khoa.nguyen@employmenthero.com>
2021-08-06 02:09:46 +0700

    Chore

--- lua/theme.lua
+++ lua/theme.lua
@@ -1,20 +1,22 @@
 vim.opt.background = 'dark'
 vim.cmd([[colorscheme nord]])
 
-vim.cmd [[autocmd ColorScheme * hi Folded guifg=#4C566A guibg=NONE gui=italic]]
-vim.cmd [[autocmd ColorScheme * hi IncSearch guifg=white guibg=NONE]]
-vim.cmd [[autocmd ColorScheme * hi Search guifg=white guibg=NONE gui=underline,bold]]
+vim.cmd[[
+augroup Theme
+  autocmd ColorScheme * hi Folded guifg=#4C566A guibg=NONE gui=italic
+  autocmd ColorScheme * hi IncSearch guifg=white guibg=NONE
+  autocmd ColorScheme * hi Search guifg=white guibg=NONE gui=underline,bold
 
--- ALE
-vim.cmd [[autocmd ColorScheme * hi ALEWarning guifg=#EBCB8B]]
-vim.cmd [[autocmd ColorScheme * hi ALEError guifg=#BF616A]]
-vim.cmd [[autocmd ColorScheme * hi ALEWarningSign guifg=#EBCB8B]]
-vim.cmd [[autocmd ColorScheme * hi ALEErrorSign guifg=#BF616A]]
+  autocmd ColorScheme * hi ALEWarning guifg=#EBCB8B
+  autocmd ColorScheme * hi ALEError guifg=#BF616A
+  autocmd ColorScheme * hi ALEWarningSign guifg=#EBCB8B
+  autocmd ColorScheme * hi ALEErrorSign guifg=#BF616A
 
--- Coc
--- vim.cmd [[autocmd ColorScheme * hi CocWarningHighlight guifg=#EBCB8B]]
--- vim.cmd [[autocmd ColorScheme * hi CocErrorHighlight guifg=#BF616A]]
--- vim.cmd [[autocmd ColorScheme * hi CocWarningSign guifg=#EBCB8B]]
--- vim.cmd [[autocmd ColorScheme * hi CocErrorSign guifg=#BF616A]]
--- vim.cmd [[autocmd ColorScheme * hi CocInfoSign guifg=#88C0D0]]
--- vim.cmd [[autocmd ColorScheme * hi CocHintSign guifg=#5E81AC]]
+  " autocmd ColorScheme * hi CocWarningHighlight guifg=#EBCB8B
+  " autocmd ColorScheme * hi CocErrorHighlight guifg=#BF616A
+  " autocmd ColorScheme * hi CocWarningSign guifg=#EBCB8B
+  " autocmd ColorScheme * hi CocErrorSign guifg=#BF616A
+  " autocmd ColorScheme * hi CocInfoSign guifg=#88C0D0
+  " autocmd ColorScheme * hi CocHintSign guifg=#5E81AC
+augroup END
+]]
