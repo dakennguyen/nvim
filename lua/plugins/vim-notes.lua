a56d8b8b478f6a4d5fa2ea75856b95edbc7c3fc1
Khoa Nguyen <khoa.nguyen@employmenthero.com>
2021-08-10 19:44:04 +0700

    reposition when open buffers

--- lua/plugins/vim-notes.lua
+++ lua/plugins/vim-notes.lua
@@ -1,3 +1,3 @@
 vim.g.notes_directories = { '~/Documents/Workspace/Thinkei/Notes' }
 vim.g.notes_tab_indents = 0
-map('n', '<space>gn', ':80vs note:home<cr>:MostRecentNote<cr><cr>', { silent = true })
+map('n', '<space>gn', ':vert botright 80 new note:home<cr>:MostRecentNote<cr><cr>', { silent = true })
--- lua/plugins/vim-test.lua
+++ lua/plugins/vim-test.lua
@@ -1,4 +1,5 @@
 vim.g["test#strategy"] = "neovim"
+vim.g["test#neovim#term_position"] = 'vert'
 
 vim.g['test#ruby#bundle_exec'] = 1
 vim.g['test#ruby#use_binstubs'] = 0
