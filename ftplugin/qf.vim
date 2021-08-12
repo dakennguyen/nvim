0881c29e1b0a8b935c709bb3e06018255e3cbfec
Khoa Nguyen <khoa.nguyen@employmenthero.com>
2021-08-12 17:00:46 +0700

    bqf

---
+++ ftplugin/qf.vim
@@ -0,0 +1,1 @@
+nnoremap <buffer> p :lua require('bqf.jump').open(false)<cr><c-w>p
--- lua/plugins/bqf.lua
+++ lua/plugins/bqf.lua
@@ -2,4 +2,8 @@ require('bqf').setup{
   preview = {
     auto_preview = false,
   },
+  func_map = {
+    split = '<c-s>',
+    ptoggleitem = '',
+  },
 }
