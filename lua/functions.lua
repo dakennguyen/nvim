aa949a08d25539f28ddd1e1b774d097ff730e218
Khoa Nguyen <khoa.nguyen@employmenthero.com>
2021-08-14 18:24:03 +0700

    Chore

--- lua/functions.lua
+++ lua/functions.lua
@@ -5,6 +5,7 @@ augroup AutoReload
   autocmd FileChangedShellPost * echohl WarningMsg | echo "File changed on disk. Buffer reloaded." | echohl None
 augroup END
 ]]
+-- }}}
 
 -- WIPE ALL REGISTERS {{{
 vim.cmd [[command! WipeReg for i in range(34,122) | silent! call setreg(nr2char(i), []) | endfor]]
