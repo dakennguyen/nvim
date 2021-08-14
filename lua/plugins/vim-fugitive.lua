8acd99d9720e6f82bc0688826966fd86142a9b8b
Khoa Nguyen <khoa.nguyen@employmenthero.com>
2021-08-14 23:57:56 +0700

    Fugitive

--- lua/plugins/vim-fugitive.lua
+++ lua/plugins/vim-fugitive.lua
@@ -8,16 +8,20 @@ end
 vim.cmd[[ command! -nargs=? Browse lua _G.open_in_browser(<q-args>) ]]
 
 vim.opt.diffopt:append { 'vertical' }
-map('n', 'gs', ':-1Gtabedit :<CR>',                                                     { silent = true }) -- Git status
+map('n', 'gs', ':tab G | tabmove -1<CR>',                                               { silent = true }) -- Git status
 map('n', 'gb', ':Git blame<CR>',                                                        { silent = true }) -- Git blame
 map('n', '<leader>gl', ':Gclog! -n 100 origin/HEAD..HEAD<cr>',                          { silent = true }) -- Git log
 map('n', '<leader>gg', ':tab Git log --oneline --graph --decorate --abbrev-commit<CR>', { silent = true }) -- Git log
-map('n', '<leader>gd', ':tab Git diff')                                                                    -- Git diff
+map('n', '<leader>gd', ':Gdiffsplit<CR>')                                                                  -- Git diff
 map('n', '<leader>gs', ':Gclog! -g stash<CR>',                                          { silent = true }) -- Git stash
 map('n', '<leader>gt', ':0Gclog!<CR>',                                                  { silent = true }) -- Git time machine
 map('n', '<leader>gx', ':GBrowse<CR>',                                                  { silent = true }) -- Git Browse
 map('v', '<leader>gx', ':GBrowse<CR>',                                                  { silent = true }) -- Git Browse in Visual mode
+
+-- vimdiff
 map('n', '<leader>gf', ':diffget //2<cr>')
 map('n', '<leader>gj', ':diffget //3<cr>')
+map('', '<leader>gh', ':diffput<cr>')
+map('', '<leader>go', ':diffget<cr>')
 
 -- vim.cmd[[autocmd User Fugitive command! -buffer -nargs=? -complete=customlist,fugitive#CompleteObject Gu Git branch -u <args>]]
