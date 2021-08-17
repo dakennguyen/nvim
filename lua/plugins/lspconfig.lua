3024ef4cb64ba172b593c8a6ae05faa57112463f
Khoa Nguyen <khoa.nguyen@employmenthero.com>
2021-08-17 20:36:46 +0700

    lspconfig: efm

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -56,8 +56,14 @@ nvim_lsp.solargraph.setup{
   }
 }
 
-nvim_lsp.tsserver.setup{
-  on_attach = on_attach,
+nvim_lsp.tsserver.setup {
+  on_attach = function(client, bufnr)
+    if client.config.flags then
+      client.config.flags.allow_incremental_sync = true
+    end
+    client.resolved_capabilities.document_formatting = false
+    on_attach(client, bufnr)
+  end,
   flags = {
     debounce_text_changes = 150,
   },
@@ -68,4 +74,63 @@ nvim_lsp.jsonls.setup {
   flags = {
     debounce_text_changes = 150,
   },
+}
+
+local function eslint_config_exists()
+  local eslintrc = vim.fn.glob(".eslintrc*", 0, 1)
+
+  if not vim.tbl_isempty(eslintrc) then
+    return true
+  end
+
+  if vim.fn.filereadable("package.json") then
+    if vim.fn.json_decode(vim.fn.readfile("package.json"))["eslintConfig"] then
+      return true
+    end
+  end
+
+  return false
+end
+
+local eslint = {
+  lintCommand = "eslint_d -f visualstudio --stdin --stdin-filename ${INPUT}",
+  lintStdin = true,
+  lintFormats = { "%f(%l,%c): %tarning %m", "%f(%l,%c): %rror %m" },
+  lintIgnoreExitCode = true,
+  formatCommand = "eslint_d --fix-to-stdout --stdin --stdin-filename=${INPUT}",
+  formatStdin = true
+}
+
+nvim_lsp.efm.setup {
+  on_attach = function(client, bufnr)
+    client.resolved_capabilities.document_formatting = true
+    client.resolved_capabilities.goto_definition = false
+
+    on_attach(client, bufnr)
+  end,
+  root_dir = function()
+    if not eslint_config_exists() then
+      return nil
+    end
+    return vim.fn.getcwd()
+  end,
+  settings = {
+    languages = {
+      javascript = {eslint},
+      javascriptreact = {eslint},
+      ["javascript.jsx"] = {eslint},
+      typescript = {eslint},
+      ["typescript.tsx"] = {eslint},
+      typescriptreact = {eslint}
+    }
+  },
+  filetypes = {
+    "javascript",
+    "javascriptreact",
+    "javascript.jsx",
+    "typescript",
+    "typescript.tsx",
+    "typescriptreact"
+  },
+  init_options = { documentFormatting = true, codeAction = true },
 }
