86dd20d45539358f37ea650f87a2500c9717ef9e
Khoa Nguyen <khoa.nguyen@employmenthero.com>
2021-08-18 17:55:28 +0700

    vim-test: term position

--- lua/plugins/vim-test.lua
+++ lua/plugins/vim-test.lua
@@ -1,5 +1,5 @@
 vim.g["test#strategy"] = "neovim"
-vim.g["test#neovim#term_position"] = 'vert'
+vim.g["test#neovim#term_position"] = 'vert botright 80'
 
 vim.g['test#ruby#bundle_exec'] = 1
 vim.g['test#ruby#use_binstubs'] = 0
