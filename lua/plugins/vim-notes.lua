894b869acb351682f6434b0b6feb34b442cc7360
Khoa Nguyen <khoa.nguyen@employmenthero.com>
2021-09-03 15:06:13 +0700

    vim-notes dir

--- lua/plugins/vim-notes.lua
+++ lua/plugins/vim-notes.lua
@@ -1,3 +1,3 @@
-vim.g.notes_directories = { '~/Documents/Workspace/Thinkei/Notes' }
+vim.g.notes_directories = { '~/Library/Mobile Documents/com~apple~CloudDocs/Notes' }
 vim.g.notes_tab_indents = 0
 map('n', '<space>gn', ':vert botright 80 new note:home<cr>:MostRecentNote<cr><cr>', { silent = true })
