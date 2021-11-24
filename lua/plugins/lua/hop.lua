966f14421b585ec9c7a6aa6aaac6d80e23561c1c
Khoa Nguyen <khoa.nguyen@employmenthero.com>
2021-11-24 23:19:25 +0700

    silent hop

--- lua/plugins/lua/hop.lua
+++ lua/plugins/lua/hop.lua
@@ -1,3 +1,3 @@
 require'hop'.setup()
 map('n', 's', ':HopChar1<CR>', { silent = true })
-map('n', '<space><space>', ':HopWord<CR>', {noremap = true})
+map('n', '<space><space>', ':HopWord<CR>', { silent = true, noremap = true })
