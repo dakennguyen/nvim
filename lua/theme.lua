ea97b33091a0746c95b53a74a5e4b28cac98be42
Khoa Nguyen <khoa.nguyen@employmenthero.com>
2021-12-02 12:40:40 +0700

    Update Diff for theme

--- lua/theme.lua
+++ lua/theme.lua
@@ -6,6 +6,11 @@ vim.cmd[[
   highlight Folded guifg=#4C566A guibg=NONE gui=italic
   highlight Search guifg=white guibg=NONE gui=underline,bold
 
+  highlight DiffAdd guifg=NONE guibg=#324125 gui=NONE
+  highlight DiffChange guifg=NONE guibg=#334963 gui=NONE
+  highlight DiffDelete guifg=NONE guibg=#BF616A gui=NONE
+  highlight DiffText guifg=NONE guibg=#446285 gui=NONE
+
   highlight DiagnosticWarn guifg=#EBCB8B
   highlight DiagnosticVirtualTextWarn guifg=#EBCB8B
   highlight DiagnosticSignWarn guifg=#EBCB8B
