21bb8cf5c77fce152a5ea84bc19c4c8d43c4db9f
Khoa Nguyen <khoa.nguyen@employmenthero.com>
2021-12-14 08:42:31 +0700

    update ruby snippets

--- after/ftplugin/ruby.lua
+++ after/ftplugin/ruby.lua
@@ -1,7 +1,8 @@
 vim.cmd[[
   iabbrev <buffer> bb byebug<esc>
-  iabbrev <buffer> rben <cmd>r ~/.dotfiles/snippet/rben.rb<cr><cmd>norm ggddG<cr>
-  iabbrev <buffer> rar <cmd>r ~/.dotfiles/snippet/rar.rb<cr><cmd>norm ggddG<cr>
+  iabbrev <buffer> rben <cmd>r ~/.dotfiles/snippet/rben.rb<cr><cmd>norm kdd<cr>
+  iabbrev <buffer> rar <cmd>r ~/.dotfiles/snippet/rar.rb<cr><cmd>norm kdd<cr>
+  iabbrev <buffer> rbundler <cmd>r ~/.dotfiles/snippet/rbundler.rb<cr><cmd>norm kdd<cr>
 ]]
 
 buf_map('n', '<leader>rr', ':vsplit term://ruby <c-r>=expand("%")<CR><CR>')
