9713121bea49d29952dd324fa847f062ea5068dc
Khoa Nguyen <dakennguyen@gmail.com>
2022-05-13 00:17:45 +0200

    Update copilot node version

--- lua/plugins/vimscript/copilot.lua
+++ lua/plugins/vimscript/copilot.lua
@@ -4,3 +4,5 @@ vim.g.copilot_filetypes = {
 
 vim.g.copilot_no_tab_map = true
 map('i', '<c-space>', 'copilot#Accept("<CR>")', { silent = true, script = true, expr = true })
+
+vim.g.copilot_node_command = '/Users/dakennguyen/.asdf/installs/nodejs/16.14.2/bin/node'
