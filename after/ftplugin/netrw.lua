89cb786d729e1cb3e253e6a2037d14554aa7772c
Khoa Nguyen <dakennguyen@gmail.com>
2023-06-08 01:52:07 +0200

    Update mappings

--- after/ftplugin/netrw.lua
+++ after/ftplugin/netrw.lua
@@ -8,5 +8,6 @@ buf_map('n', '<leader>0', '<Plug>NetrwRefresh', { noremap = false })
 buf_map('v', 'mf', ':norm mf<cr>')
 
 buf_map('n', '<space>gn', [[:nnoremap <lt>silent> <lt>space>gn :vert botright 80 new <c-r><c-f><lt>cr><cr>]], { noremap = false, silent = true })
-buf_map('n', '<space>gr', [[:nnoremap <lt>silent> <lt>space>gr :tabnew <c-r><c-f><lt>cr><cr>]],             { noremap = false, silent = true })
+buf_map('n', '<space>gh', [[:nnoremap <lt>silent> <lt>space>gh :vert new <c-r><c-f><lt>cr><cr>]],             { noremap = false, silent = true })
+buf_map('n', '<space>gr', [[:nnoremap <lt>silent> <lt>space>gr :tabnew <c-r><c-f><lt>cr><cr>]],               { noremap = false, silent = true })
 buf_map('n', '<leader>fp', [[:!open -a 'google chrome' '<c-r><c-f>'<cr>]],                                    { noremap = false, silent = true })
--- lua/plugins/tpope/dadbod.lua
+++ lua/plugins/tpope/dadbod.lua
@@ -1,11 +1,11 @@
 return {
   'https://tpope.io/vim/dadbod.git',
   cmd = 'DB',
-  keys = { '<leader>dr', '<leader>dd', '<leader>dt' },
+  keys = { '<space>dr', '<space>dd', '<space>dt' },
   config = function()
-    map('', '<leader>dr', ':DB g:redis')
-    map('', '<leader>dd', ':DB g:dbdev')
-    map('', '<leader>dt', ':DB g:dbtest')
+    map('', '<space>dr', ':DB g:redis')
+    map('', '<space>dd', ':DB g:dbdev')
+    map('', '<space>dt', ':DB g:dbtest')
 
     vim.g['redis'] = "redis:localhost:6379"
   end,
