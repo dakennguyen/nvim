a55c255f617c02dc2f85a8b4e3cc578439bfe5ae
Khoa Nguyen <dakennguyen@gmail.com>
2023-06-08 02:05:19 +0200

    Do not lazy load vinegar

--- lua/plugins/tpope/misc.lua
+++ lua/plugins/tpope/misc.lua
@@ -5,7 +5,7 @@ return {
   { 'https://tpope.io/vim/surround.git', init = lazy_load 'surround' },
   { 'https://tpope.io/vim/rhubarb.git', event = 'VeryLazy' },
   { 'https://tpope.io/vim/commentary.git', event = 'VeryLazy' },
-  { 'https://tpope.io/vim/vinegar.git', event = 'VeryLazy' },
+  { 'https://tpope.io/vim/vinegar.git', lazy = false },
   { 'https://tpope.io/vim/bundler.git', ft = 'ruby' },
   { 'https://tpope.io/vim/repeat.git', init = lazy_load 'repeat', branch = 'no-feedkeys' },
   { 'dakennguyen/vim-unimpaired', init = lazy_load 'vim-unimpaired' },
