dc01d923e6290e8c684a7205d1424506d1f44d0d
Khoa Nguyen <dakennguyen@gmail.com>
2023-06-26 22:30:01 +0200

    `vsnip`: config snippets

--- after/ftplugin/ruby.lua
+++ after/ftplugin/ruby.lua
@@ -1,8 +1,5 @@
 vim.cmd[[
   iabbrev <buffer> bb byebug<esc>
-  iabbrev <buffer> rben <cmd>r ~/.dotfiles/snippet/rben.rb<cr><cmd>norm kdd<cr>
-  iabbrev <buffer> rar <cmd>r ~/.dotfiles/snippet/rar.rb<cr><cmd>norm kdd<cr>
-  iabbrev <buffer> rbundler <cmd>r ~/.dotfiles/snippet/rbundler.rb<cr><cmd>norm kdd<cr>
 ]]
 
 buf_map('n', '`<cr>', ':vsplit term://ruby <c-r>=expand("%")<CR><CR>')
