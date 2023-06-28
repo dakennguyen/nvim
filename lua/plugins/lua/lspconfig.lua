226c5dbfd7cce32688702ca62580cc5159596e55
Khoa Nguyen <dakennguyen@gmail.com>
2023-06-28 09:13:48 +0200

    `nvim`: add lua_ls

--- lua/plugins/lua/lspconfig.lua
+++ lua/plugins/lua/lspconfig.lua
@@ -198,6 +198,34 @@ return {
       flags = lsp_flags,
     }
 
+    -- git clone https://github.com/LuaLS/lua-language-server
+    -- ./make.sh
+    nvim_lsp.lua_ls.setup {
+      capabilities = capabilities,
+      on_attach = on_attach,
+      flags = lsp_flags,
+      settings = {
+        Lua = {
+          runtime = {
+            -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+            version = 'LuaJIT',
+          },
+          diagnostics = {
+            -- Get the language server to recognize the `vim` global
+            globals = {'vim'},
+          },
+          workspace = {
+            -- Make the server aware of Neovim runtime files
+            library = vim.api.nvim_get_runtime_file("", true),
+          },
+          -- Do not send telemetry data containing a randomized but unique identifier
+          telemetry = {
+            enable = false,
+          },
+        },
+      },
+    }
+
     -- `npm install -g eslint_d`
     -- `npm install -g prettier`
     -- `brew install efm-langserver`
