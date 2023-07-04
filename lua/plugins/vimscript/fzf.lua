732b858c7d1e6d55bbf75173ecf916e229e76b59
Khoa Nguyen <dakennguyen@gmail.com>
2023-07-05 00:31:48 +0200

    `nvim`: update fzf config

--- lua/plugins/vimscript/fzf.lua
+++ lua/plugins/vimscript/fzf.lua
@@ -13,10 +13,94 @@ return {
       .. "ctrl-u:preview-page-up,tab:toggle-out,shift-tab:toggle-in"
 
     vim.cmd [[
+      let s:TYPE = {
+        \ 'bool': type(0),
+        \ 'dict': type({}),
+        \ 'funcref': type(function('call')),
+        \ 'string': type(''),
+        \ 'list': type([])
+      \ }
+      let s:default_action = {}
+
       function! BuildQuickfixList(lines)
         call setqflist(map(copy(a:lines), '{ "filename": v:val }'))
         copen
       endfunction
+
+      function! s:action_for(key, ...)
+        let default = a:0 ? a:1 : ''
+        let Cmd = get(get(g:, 'fzf_action', s:default_action), a:key, default)
+        return type(Cmd) == s:TYPE.string ? Cmd : default
+      endfunction
+
+      function! s:find_open_window(b)
+        let [tcur, tcnt] = [tabpagenr() - 1, tabpagenr('$')]
+        for toff in range(0, tabpagenr('$') - 1)
+          let t = (tcur + toff) % tcnt + 1
+          let buffers = tabpagebuflist(t)
+          for w in range(1, len(buffers))
+            let b = buffers[w - 1]
+            if b == a:b
+              return [t, w]
+            endif
+          endfor
+        endfor
+        return [0, 0]
+      endfunction
+
+      function! s:jump(t, w)
+        execute a:t.'tabnext'
+        execute a:w.'wincmd w'
+      endfunction
+
+      function! s:buffers_sink(lines, query, fullscreen)
+        if len(a:lines) < 3
+          " echo "lines0:".string(a:lines)
+          return
+        endif
+        let b = matchstr(a:lines[2], '\[\zs[0-9]*\ze\]')
+        let bufname=split(a:lines[2])[-1]
+        let action = a:lines[1]
+        " echo "lines0.5:".string(a:lines).",b:".b."(".string(bufname).")"
+        if empty(action)
+          " echo "lines1:".string(a:lines).",bdelete:".b."(".bufname.")"
+          let [t, w] = s:find_open_window(b)
+          if t
+            call s:jump(t, w)
+            return
+          endif
+          execute 'buffer' b
+          echo "Switch to '".bufname."'"
+          return
+        endif
+        if action==?'ctrl-d'
+          execute 'bdelete' b
+          " echo "lines2:".string(a:lines).",bdelete:".b."(".bufname.")"
+          call BuffersWithDeleteAction(a:query, a:fullscreen)
+        else
+          let cmd = s:action_for(action)
+          " echo "lines3:".string(a:lines).",cmd:".string(cmd).",b:".b."(".string(bufname).")"
+          if !empty(cmd)
+            execute 'silent' cmd
+          endif
+          execute 'buffer' b
+        endif
+      endfunction
+
+      function! BuffersWithDeleteAction(query, fullscreen)
+        let spec = { 'sink*': {lines -> s:buffers_sink(lines, a:query, a:fullscreen)},
+              \ 'options': [
+              \   '--print-query',
+              \   '--prompt', 'Buffer> '
+              \ ],
+              \ 'placeholder': '{1}'
+              \ }
+        let spec._action = get(g:, 'fzf_action', s:default_action)
+        call add(spec.options, '--expect=ctrl-d,'.join(keys(spec._action), ','))
+        call fzf#vim#buffers(a:query, fzf#vim#with_preview(spec), a:fullscreen)
+      endfunction
+
+      command! -bar -bang -nargs=? -complete=dir FzfxBuffers call BuffersWithDeleteAction(<q-args>, <bang>0)
     ]]
 
     vim.g.fzf_history_dir = "~/.cache/fzf/fzf-history"
@@ -60,12 +144,13 @@ return {
 
     map("n", "<space>ff", ":Files<CR>", { silent = true })
     map("n", "<space>fd", ":Files %:p:h<CR>", { silent = true })
-    map("n", "<space>fb", ":Buffers<CR>", { silent = true })
+    map("n", "<space>fb", ":FzfxBuffers<CR>", { silent = true })
     map("n", "<space>fo", ":History<CR>", { silent = true })
     map("n", "<space>fh", ":Helptags<CR>", { silent = true })
-    map("n", "<space>fk", ":Maps<CR>", { silent = true })
+    map("n", "<space>fm", ":Maps<CR>", { silent = true })
     map("n", "<space>fw", ":Rg <C-R><C-W><CR>", { silent = true })
     map("v", "<space>fw", [[y:Rg <C-R>=escape(@",'/\')<CR><CR>]], { silent = true })
+    map("n", "<space>fc", ":History:<CR>")
 
     map("n", "//", ":BLines<CR>")
     map("n", "\\", ":Rg ")
