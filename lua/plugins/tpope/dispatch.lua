f5d85c8a60670c135b4b15757f44468c31c8b9d6
Khoa Nguyen <dakennguyen@gmail.com>
2023-07-14 20:56:34 +0200

    `nvim`: update dispatch config

--- lua/plugins/tpope/dispatch.lua
+++ lua/plugins/tpope/dispatch.lua
@@ -1,12 +1,15 @@
 return {
   "https://tpope.io/vim/dispatch.git",
-  lazy = false,
+  event = "VeryLazy",
   config = function()
     map("n", "<leader>C", ":Copen<cr>", { silent = true })
+    map("n", "<space>C", ":Copen!<cr>", { silent = true })
+
     vim.g["dispatch_no_tmux_make"] = true
     vim.g["dispatch_no_tmux_start"] = true
     vim.g["dispatch_compilers"] = {
       ["python -m pytest"] = "pytest",
+      ["poetry run"] = "",
       ["bundle exec spring"] = "",
       ["bundle exec"] = "",
     }
