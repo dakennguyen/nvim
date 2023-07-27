28af4beca905344dfd2efb65ed7f01b5339ec160
Khoa Nguyen <dakennguyen@gmail.com>
2023-07-28 00:33:28 +0200

    Update treesitter config

--- lua/plugins/lua/treesitter.lua
+++ lua/plugins/lua/treesitter.lua
@@ -31,15 +31,6 @@ return {
     highlight = {
       enable = true,
     },
-    incremental_selection = {
-      enable = false,
-      keymaps = {
-        init_selection = "gnn",
-        node_incremental = "grn",
-        scope_incremental = "grc",
-        node_decremental = "grm",
-      },
-    },
     indent = {
       enable = true,
       disable = { "ruby", "go" },
@@ -51,20 +42,21 @@ return {
     context_commentstring = {
       enable = true,
     },
+    -- "nvim-treesitter/nvim-treesitter-textobjects"
     textobjects = {
       select = {
         enable = true,
         lookahead = true,
         keymaps = {
-          ["af"] = "@function.outer",
-          ["if"] = "@function.inner",
+          ["am"] = "@function.outer",
+          ["im"] = "@function.inner",
           ["ac"] = "@class.outer",
           ["ic"] = "@class.inner",
         },
       },
       move = {
         enable = true,
-        set_jumps = false,
+        set_jumps = true,
         goto_next_start = {
           ["]m"] = "@function.outer",
           ["]]"] = "@class.outer",
