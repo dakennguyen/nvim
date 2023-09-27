08f6567e7a96b029089c2c0f0b4f906e8807feee
Khoa Nguyen <dakennguyen@gmail.com>
2023-09-27 14:10:27 +0200

    Update nord theme

--- .config/nvim/lua/themes/nord.lua
+++ .config/nvim/lua/themes/nord.lua
@@ -2,13 +2,13 @@ vim.opt.background = "dark"
 vim.g.nord_italic = true
 require("nord").set()
 
-_G.highlight("Folded", { fg = "#4C566A", bg = "NONE", style = "italic" })
-_G.highlight("Search", { fg = "white", bg = "NONE", style = "underline,bold" })
+-- _G.highlight("Folded", { fg = "#4C566A", bg = "NONE", style = "italic" })
+-- _G.highlight("Search", { fg = "white", bg = "NONE", style = "underline,bold" })
 
-_G.highlight("DiffAdd", { fg = "NONE", bg = "#324125", style = "NONE" })
-_G.highlight("DiffChange", { fg = "NONE", bg = "#334963", style = "NONE" })
-_G.highlight("DiffDelete", { fg = "NONE", bg = "#BF616A", style = "NONE" })
-_G.highlight("DiffText", { fg = "NONE", bg = "#446285", style = "NONE" })
+-- _G.highlight("DiffAdd", { fg = "NONE", bg = "#324125", style = "NONE" })
+-- _G.highlight("DiffChange", { fg = "NONE", bg = "#334963", style = "NONE" })
+-- _G.highlight("DiffDelete", { fg = "NONE", bg = "#BF616A", style = "NONE" })
+-- _G.highlight("DiffText", { fg = "NONE", bg = "#446285", style = "NONE" })
 
 _G.highlight("DiagnosticWarn", { fg = "#EBCB8B" })
 _G.highlight("DiagnosticVirtualTextWarn", { fg = "#EBCB8B" })
@@ -28,5 +28,10 @@ return {
     yellow = "#EBCB8B",
     inactive = "#81879C",
     dark = "#FFFFFF",
+  },
+  gitsigns = {
+    add_bg = "#324125",
+    change_bg = "#334963",
+    delete_bg = "#BF616A",
   },
 }
