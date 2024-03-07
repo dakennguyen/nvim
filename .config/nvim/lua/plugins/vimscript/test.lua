490960483e7c2df30ec54c98b3343fee9d57b349
Khoa Nguyen <dakennguyen@gmail.com>
2024-03-07 11:10:54 +0100

    Update neovim test strategy

--- .config/nvim/lua/plugins/vimscript/test.lua
+++ .config/nvim/lua/plugins/vimscript/test.lua
@@ -12,8 +12,10 @@ return {
 
     -- vim.g["test#custom_strategies"] = { buffer = vim.fn["BufferStrategy"] }
     vim.g["test#strategy"] = "dispatch"
+    vim.g["test#preserve_screen"] = 0
     vim.g["test#neovim#term_position"] = "split"
     vim.g["test#neovim#start_normal"] = 1
+    vim.g["test#neovim_sticky#reopen_window"] = 1
 
     vim.g["test#ruby#bundle_exec"] = 1
     vim.g["test#ruby#use_binstubs"] = 0
