a5a3115f479853e3ecd813bc3636f0cefedc6948
Khoa Nguyen <dakennguyen@gmail.com>
2024-05-08 00:46:30 +0200

    Install arrow

---
+++ .config/nvim/lua/plugins/lua/arrow.lua
@@ -0,0 +1,10 @@
+return {
+  "otavioschwanck/arrow.nvim",
+  lazy = false,
+  opts = {
+    always_show_path = true,
+    show_icons = true,
+    leader_key = "<leader>,",
+    buffer_leader_key = "m",
+  }
+}
