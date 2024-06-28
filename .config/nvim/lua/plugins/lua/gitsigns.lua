ccb19d664c2e17356bd8fdae554abb5e0ebc57b1
Khoa Nguyen <dakennguyen@gmail.com>
2024-06-28 15:22:03 +0200

    Update gitsigns

--- .config/nvim/lua/plugins/lua/gitsigns.lua
+++ .config/nvim/lua/plugins/lua/gitsigns.lua
@@ -4,11 +4,11 @@ return {
   init = _G.lazy_load "gitsigns.nvim",
   opts = {
     signs = {
-      add = { hl = "GitSignsAdd", text = "▌", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-      change = { hl = "GitSignsChange", text = "▌", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-      delete = { hl = "GitSignsDelete", text = "▁", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-      topdelete = { hl = "GitSignsDelete", text = "▔", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-      changedelete = { hl = "GitSignsChange", text = "▌", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+      add = { text = "▌" },
+      change = { text = "▌" },
+      delete = { text = "▁" },
+      topdelete = { text = "▔" },
+      changedelete = { text = "▌" },
     },
     signcolumn = false,
     numhl = true,
