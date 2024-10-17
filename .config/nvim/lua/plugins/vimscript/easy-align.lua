ce243eca1a68609d49f63c5ad1ccb4bd3d078349
Khoa Nguyen <dakennguyen@gmail.com>
2024-10-17 23:42:00 +0200

    Remove gv and easy-align

--- .config/nvim/lua/plugins/vimscript/easy-align.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "junegunn/vim-easy-align",
-  keys = { "ga", { "ga", mode = "x" } },
-  config = function()
-    map("n", "ga", "<Plug>(EasyAlign)", { remap = true })
-    map("x", "ga", "<Plug>(EasyAlign)", { remap = true })
-  end,
-}
