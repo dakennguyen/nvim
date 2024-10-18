4482a94736e51f88e350b69e91d5e100580b882b
Khoa Nguyen <dakennguyen@gmail.com>
2024-10-18 10:41:10 +0200

    feat: add dap ui

--- .config/nvim/lua/plugins/lua/dap.lua
+++ .config/nvim/lua/plugins/lua/dap.lua
@@ -1,6 +1,9 @@
 return {
   "mfussenegger/nvim-dap",
-  dependencies = "leoluz/nvim-dap-go",
+  dependencies = {
+    "leoluz/nvim-dap-go",
+    { "rcarriga/nvim-dap-ui", dependencies = "nvim-neotest/nvim-nio" },
+  },
   keys = {
     {
       "<F9>",
@@ -38,6 +41,10 @@ return {
       function() require("dapui").eval(nil, { enter = true }) end,
     },
     {
+      "<leader>du",
+      function() require("dapui").toggle() end,
+    },
+    {
       "<leader>dE",
       function() require("dap").repl.open() end,
     },
@@ -58,5 +65,8 @@ return {
       function() require("dap").step_out() end,
     },
   },
-  config = function() require("dap-go").setup() end,
+  config = function()
+    require("dapui").setup()
+    require("dap-go").setup()
+  end,
 }
