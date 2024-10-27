4b86481b62b81133c746335881c452621848e82b
Khoa Nguyen <dakennguyen@gmail.com>
2024-10-27 21:26:07 +0100

    Turn off python3 and node providers

--- .config/nvim/lua/config/settings.lua
+++ .config/nvim/lua/config/settings.lua
@@ -2,6 +2,8 @@ local o = vim.opt
 local g = vim.g
 
 g.mapleader = ","
+g.loaded_python3_provider = 0
+g.loaded_node_provider = 0
 g.loaded_ruby_provider = 0
 g.loaded_perl_provider = 0
 o.swapfile = false
