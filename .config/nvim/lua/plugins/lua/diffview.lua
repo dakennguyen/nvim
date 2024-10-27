5186264bf08beed96ff731b7ed9c9cb6101751a5
Khoa Nguyen <dakennguyen@gmail.com>
2024-10-27 20:51:49 +0100

    Use diffview

---
+++ .config/nvim/lua/plugins/lua/diffview.lua
@@ -0,0 +1,16 @@
+return {
+  "sindrets/diffview.nvim",
+  cmd = { "DiffviewOpen", "DiffviewFileHistory" },
+  keys = {
+    { "<leader>gd", ":DiffviewFileHistory %<CR>", silent = true },
+    { "<space>gd", ":DiffviewOpen origin/HEAD...HEAD --imply-local<CR>", silent = true },
+    { mode = "v", "<leader>gt", ":DiffviewFileHistory<CR>", silent = true },
+  },
+  opts = {
+    file_panel = {
+      win_config = {
+        width = 70,
+      },
+    },
+  },
+}
