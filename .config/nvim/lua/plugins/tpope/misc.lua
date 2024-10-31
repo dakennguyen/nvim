34c222d4221897ca838e2a97eb5ccf23a1c630c0
Khoa Nguyen <khoa.nguyen@check24.de>
2024-10-31 15:10:11 +0100

    Use vim-fubitive

--- .config/nvim/lua/plugins/tpope/misc.lua
+++ .config/nvim/lua/plugins/tpope/misc.lua
@@ -1,6 +1,5 @@
 return {
   { "tpope/vim-surround", init = _G.lazy_load "vim-surround" },
-  { "tpope/vim-rhubarb", init = _G.lazy_load "vim-rhubarb" },
   { "tpope/vim-bundler", event = "BufWinEnter Gemfile*" },
   { "tpope/vim-repeat", init = _G.lazy_load "vim-repeat" },
   { "tpope/vim-sleuth", lazy = false },
