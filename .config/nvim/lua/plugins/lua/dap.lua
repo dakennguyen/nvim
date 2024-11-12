6cf0eca1d0ce231fff035cf297cea6a8ef9909b1
Khoa Nguyen <dakennguyen@gmail.com>
2024-11-12 14:45:40 +0100

    Update config for cpp

--- .config/nvim/lua/plugins/lua/dap.lua
+++ .config/nvim/lua/plugins/lua/dap.lua
@@ -94,5 +94,25 @@ return {
   config = function()
     require("dapui").setup()
     require("dap-go").setup()
+
+    local dap = require "dap"
+    dap.adapters.lldb = {
+      type = "executable",
+      command = "/opt/homebrew/opt/llvm/bin/lldb-dap",
+      name = "lldb",
+    }
+    dap.configurations.cpp = {
+      {
+        name = "Launch",
+        type = "lldb",
+        request = "launch",
+        program = function()
+          return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+        end,
+        cwd = "${workspaceFolder}",
+        stopOnEntry = false,
+        args = {},
+      },
+    }
   end,
 }
