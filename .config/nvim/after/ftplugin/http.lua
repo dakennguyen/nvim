367bf386a5d118d5e10378eea33e144a635eeccf
Khoa Nguyen <dakennguyen@gmail.com>
2024-12-04 12:08:03 +0100

    chore(config): use old rest version

--- .config/nvim/after/ftplugin/http.lua
+++ .config/nvim/after/ftplugin/http.lua
@@ -1,6 +1,6 @@
 vim.opt_local.commentstring = "#%s"
 
-vim.b["dispatch"] = ":Rest run"
-buf_map("n", "<leader>ry", ":Rest curl yank<cr>")
+vim.b["dispatch"] = ":lua require('rest-nvim').run()"
+buf_map("n", "<leader>ry", [[:lua require('rest-nvim').run(true)<cr>]], { silent = true })
 
-map("n", "<space>rl", ":Rest last<cr>", { silent = true })
+map("n", "<space>rl", [[:lua require('rest-nvim').last()<cr>]], { silent = true })
