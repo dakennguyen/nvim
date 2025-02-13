07419857eeb577784c9a6112dda071facf038088
Khoa Nguyen <dakennguyen@gmail.com>
2025-02-14 00:31:29 +0100

    feat: add snacks, remove fzf-lua/project/bqf/vim-indent-object

--- .config/nvim/after/ftplugin/qf.lua
+++ .config/nvim/after/ftplugin/qf.lua
@@ -1,14 +1,3 @@
-vim.cmd [[
-  " let &l:winheight = 12
-
-  function! s:open()
-  lua require("bqf.qfwin.handler").open(false)
-  wincmd p
-  endfunction
-
-  nnoremap <silent><buffer> p :call <sid>open()<CR>
-]]
-
 vim.opt_local.wrap = true
 
 buf_map("n", "<leader>z", ":tab split<cr>", { silent = true })
