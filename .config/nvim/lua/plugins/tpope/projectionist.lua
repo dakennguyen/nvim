937333a895aea81f60020929bbe65db31488bb49
Khoa Nguyen <khoa.nguyen@check24.de>
2025-02-19 16:49:11 +0100

    wip: projectionist

--- .config/nvim/lua/plugins/tpope/projectionist.lua
+++ .config/nvim/lua/plugins/tpope/projectionist.lua
@@ -5,89 +5,129 @@ return {
     { "<space>6", ":A<CR>", silent = true },
   },
   config = function()
-    vim.g.projectionist_heuristics = {
+    local rails = {
       ["*"] = {
-        ["*.http"] = { alternate = ".env" },
+        console = "bundle exec rails console",
       },
-      -- Ruby on Rails
-      ["bin/rails"] = {
-        ["*"] = {
-          console = "bundle exec rails console",
-        },
-        ["*.rb"] = {
-          dispatch = 'bundle exec rails runner "{file}"',
-        },
-        ["lib/*.rb"] = {
-          alternate = "spec/lib/{}_spec.rb",
-          type = "source",
-        },
-        ["lib/*.rake"] = {
-          alternate = "spec/lib/{}_rake_spec.rb",
-          type = "source",
-        },
-        ["app/*.rb"] = {
-          alternate = "spec/{}_spec.rb",
-          type = "source",
-        },
-        ["app/controllers/*_controller.rb"] = {
-          alternate = {
-            "spec/requests/{}_spec.rb",
-            "spec/controllers/{}_controller_spec.rb",
-          },
-          related = {
-            "app/models/{singular}.rb",
-            "app/views/{basename}",
-            "app/policies/{singular}_policy.rb",
-            "app/helpers/{singular}_helper.rb",
-          },
-          type = "controller",
-        },
-        ["app/models/*.rb"] = {
-          related = {
-            "app/controllers/{plural}_controller.rb",
-            "app/views/{plural}",
-            "app/policies/{}_policy.rb",
-          },
-          type = "model",
-        },
-        ["app/views/*"] = {
-          alternate = "app/controllers/{dirname}_controller.rb",
-          type = "view",
-        },
-        ["app/policies/*_policy.rb"] = {
-          related = {
-            "app/models/{}.rb",
-            "app/views/{plural}",
-            "app/controllers/{plural}_controller.rb",
-          },
-          type = "policy",
-        },
-        ["app/helpers/*_helper.rb"] = {
-          related = {
-            "app/models/{}.rb",
-            "app/views/{plural}",
-            "app/controllers/{plural}_controller.rb",
-            "app/policies/{}_policy.rb",
-          },
-          type = "helper",
-        },
-        ["spec/*_spec.rb"] = {
-          alternate = {
-            "app/{}.rb",
-            "{}.rb",
-          },
-          type = "test",
-        },
-        ["spec/*_rake_spec.rb"] = {
-          alternate = "{}.rake",
-          type = "test",
-        },
-        ["spec/requests/*_spec.rb"] = {
-          command = "request",
-          alternate = "app/controllers/{}_controller.rb",
-          type = "test",
-        },
+      ["db/*schema.rb"] = {
+        type = "schema",
       },
+      ["config/*.yml"] = {
+        type = "env",
+      },
+      ["config/routes.rb"] = {
+        type = "routes",
+      },
+      ["*.rb"] = {
+        dispatch = 'bundle exec rails runner "{file}"',
+      },
+      ["lib/*.rb"] = {
+        alternate = "spec/lib/{}_spec.rb",
+        type = "source",
+      },
+      ["lib/*.rake"] = {
+        alternate = "spec/lib/{}_rake_spec.rb",
+        type = "source",
+      },
+      ["app/*.rb"] = {
+        alternate = "spec/{}_spec.rb",
+        type = "source",
+      },
+      ["app/controllers/*_controller.rb"] = {
+        alternate = {
+          "spec/requests/{}_spec.rb",
+          "spec/controllers/{}_controller_spec.rb",
+        },
+        related = {
+          "app/models/{singular}.rb",
+          "app/views/{basename}",
+          "app/policies/{singular}_policy.rb",
+          "app/helpers/{singular}_helper.rb",
+        },
+        type = "controller",
+      },
+      ["app/models/*.rb"] = {
+        related = {
+          "app/controllers/{plural}_controller.rb",
+          "app/views/{plural}",
+          "app/policies/{}_policy.rb",
+        },
+        type = "model",
+      },
+      ["app/views/*"] = {
+        alternate = "app/controllers/{dirname}_controller.rb",
+        type = "view",
+      },
+      ["app/policies/*_policy.rb"] = {
+        related = {
+          "app/models/{}.rb",
+          "app/views/{plural}",
+          "app/controllers/{plural}_controller.rb",
+        },
+        type = "policy",
+      },
+      ["app/helpers/*_helper.rb"] = {
+        related = {
+          "app/models/{}.rb",
+          "app/views/{plural}",
+          "app/controllers/{plural}_controller.rb",
+          "app/policies/{}_policy.rb",
+        },
+        type = "helper",
+      },
+      ["spec/*_spec.rb"] = {
+        alternate = {
+          "app/{}.rb",
+          "{}.rb",
+        },
+        type = "test",
+      },
+      ["spec/*_rake_spec.rb"] = {
+        alternate = "{}.rake",
+        type = "test",
+      },
+      ["spec/requests/*_spec.rb"] = {
+        command = "request",
+        alternate = "app/controllers/{}_controller.rb",
+        type = "test",
+      },
+    }
+
+    local ruby = {
+      ["Gemfile"] = {
+        alternate = "Gemfile.lock",
+        type = "specs",
+      },
+      ["Gemfile.lock"] = {
+        alternate = "Gemfile",
+      },
+    }
+
+    local php = {
+      ["composer.json"] = {
+        alternate = "composer.lock",
+        type = "specs",
+      },
+      ["composer.lock"] = {
+        alternate = "composer.json",
+      }
+    }
+
+    local javascript = {
+      ["package.json"] = {
+        alternate = "package-lock.json",
+        type = "specs",
+      },
+      ["package-lock.json"] = {
+        alternate = "package.json",
+      }
+    }
+
+    vim.g.projectionist_heuristics = {
+      ["bin/rails"] = rails,
+      ["Gemfile"] = ruby,
+      ["composer.json"] = php,
+      ["package.json"] = javascript,
     }
   end,
 }
