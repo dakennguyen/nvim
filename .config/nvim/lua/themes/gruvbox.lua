5cf0379429e81fd78413a0138a71621ca21c0601
Khoa Nguyen <dakennguyen@gmail.com>
2025-02-23 23:16:25 +0100

    fix: fix git and sign color

--- .config/nvim/lua/themes/gruvbox.lua
+++ .config/nvim/lua/themes/gruvbox.lua
@@ -5,27 +5,11 @@ M.specs = {
   lazy = false,
   priority = 1000,
   config = function()
-    local gruvbox = require "gruvbox"
-
-    gruvbox.setup {
-      overrides = {
-        SignColumn = { bg = gruvbox.palette.light0 },
-      },
-    }
-
+    vim.cmd "hi! link SignColumn Normal"
     vim.cmd "colorscheme gruvbox"
   end,
 }
 
-function M.colors()
-  local utils = require "utils"
-  return {
-    gitsigns = {
-      add_bg = utils.get_hl("DiffAdd").bg,
-      change_bg = utils.get_hl("DiffChange").bg,
-      delete_bg = utils.get_hl("DiffDelete").bg,
-    },
-  }
-end
+function M.colors() return {} end
 
 return M
