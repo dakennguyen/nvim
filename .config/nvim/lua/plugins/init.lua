8dcebc46a894e61780062072ac008a5e084583d1
Khoa Nguyen <khoa.nguyen@tum.de>
2025-03-08 13:05:16 +0100

    Update config

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,12 +1,10 @@
 return {
   -- Group: Theme
   require("themes").specs,
-  "nvim-tree/nvim-web-devicons",
   require "plugins.lua.lualine",
 
   -- Group: TPope
   require "plugins.tpope.misc",
-  require "plugins.tpope.dadbod",
   require "plugins.tpope.dispatch",
   require "plugins.tpope.fugitive",
   require "plugins.tpope.projectionist",
@@ -15,7 +13,6 @@ return {
   require "plugins.lua.treesitter",
   require "plugins.lua.lspconfig",
   require "plugins.vimscript.gutentags",
-  require "plugins.lua.context-commentstring",
 
   -- Group: Completion
   require "plugins.lua.cmp",
@@ -28,7 +25,6 @@ return {
 
   -- Group: Editing support
   require "plugins.lua.autopairs",
-  require "plugins.lua.autotag",
   require "plugins.lua.treesj",
   require "plugins.lua.treesitter-textobjects",
 
@@ -43,7 +39,6 @@ return {
   require "plugins.lua.gitsigns",
 
   -- Group: Miscellaneous
-  require "plugins.lua.colorizer",
   require "plugins.lua.kulala",
   require "plugins.lua.snacks",
   require "plugins.vimscript.slime",
--- .config/nvim/lua/plugins/lua/dap.lua
+++ .config/nvim/lua/plugins/lua/dap.lua
@@ -72,15 +72,15 @@ return {
     -- Configurations
     -- ======================================
     dap.configurations.cpp = {
-      {
-        name = "Launch (lldb)",
-        type = "lldb",
-        request = "launch",
-        program = function() return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file") end,
-        cwd = "${workspaceFolder}",
-        stopOnEntry = false,
-        args = {},
-      },
+      -- {
+      --   name = "Launch (lldb)",
+      --   type = "lldb",
+      --   request = "launch",
+      --   program = function() return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file") end,
+      --   cwd = "${workspaceFolder}",
+      --   stopOnEntry = false,
+      --   args = {},
+      -- },
       {
         name = "Launch (cppdbg)",
         type = "cppdbg",
@@ -89,6 +89,18 @@ return {
         cwd = "${workspaceFolder}",
         stopAtEntry = true,
       },
+      -- {
+      --   name = 'Attach to gdbserver :1234',
+      --   type = 'cppdbg',
+      --   request = 'launch',
+      --   MIMode = 'gdb',
+      --   miDebuggerServerAddress = 'localhost:1234',
+      --   miDebuggerPath = '/usr/bin/gdb',
+      --   cwd = '${workspaceFolder}',
+      --   program = function()
+      --     return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+      --   end,
+      -- },
     }
 
     -- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
