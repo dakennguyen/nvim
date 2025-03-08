04dbc37180638981f8941acdda4cafd61e964e0e
Khoa Nguyen <dakennguyen@gmail.com>
2025-03-08 21:45:45 +0100

    Update color

--- .config/nvim/lua/themes/tokyonight.lua
+++ .config/nvim/lua/themes/tokyonight.lua
@@ -14,7 +14,26 @@ function M.colors()
 
   _G.highlight("CursorLineNR", { fg = palette.fg_gutter })
 
-  return {}
+  return {
+    lualine = {
+      default_fg = palette.fg,
+      default_bg = palette.bg,
+      light = palette.fg_dark,
+      visual = palette.orange,
+      insert = palette.blue,
+      replace = palette.red,
+      command = palette.magenta,
+      op = palette.green,
+      yellow = palette.yellow,
+      inactive = palette.fg_dark,
+      strong = palette.fg,
+    },
+    gitsigns = {
+      add_fg = palette.green,
+      change_fg = palette.blue,
+      delete_fg = palette.red,
+    },
+  }
 end
 
 return M
