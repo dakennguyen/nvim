979c59ef448d620cac1a7658302f36168b21032b
Khoa Nguyen <dakennguyen@gmail.com>
2025-03-15 08:37:34 +0100

    Use dispatch and projectionist to run tests

--- .config/nvim/lua/plugins/tpope/dispatch.lua
+++ .config/nvim/lua/plugins/tpope/dispatch.lua
@@ -4,6 +4,11 @@ return {
   keys = {
     { "<leader>C", ":Copen<cr>", silent = true },
     { "<space>C", ":Copen!<cr>", silent = true },
+    { mode = "v", "`<space>", ":Dispatch<space>" },
+    { mode = "v", "`<cr>", ":Dispatch<cr>" },
+    { "t<c-n>", "mT:.Dispatch<cr>" },
+    { "t<c-l>", ":Copen|Dispatch<cr>" },
+    { "t<c-g>", "'T" },
   },
   config = function()
     vim.g["dispatch_no_tmux_start"] = true
