50a94dc78cce845d3a9a7898fa68306c8f2f3373
Khoa Nguyen <dakennguyen@gmail.com>
2025-03-19 15:42:29 +0100

    chore: restructure code

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,9 +5,7 @@ require "globals"
 require "config.options"
 require "config.keymaps"
 require "config.autocmds"
-
 require "config.lazy"
-require "plugins"
 
 vim.notify(vim.fn.printf("Loaded in %.0fms", vim.fn.reltimefloat(vim.fn.reltime(start_time)) * 1000))
 
