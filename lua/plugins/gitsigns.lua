dc4f0d710d6ba4d45d13d8c45936125121b7e21a
Khoa Nguyen <dakennguyen@gmail.com>
2025-03-22 17:53:29 +0100

    Lock to v0.8.1

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,6 +1,6 @@
 return {
   "lewis6991/gitsigns.nvim",
-  tag = "v1.0.1",
+  tag = "v0.8.1",
   init = _G.lazy_load "gitsigns.nvim",
   opts = {
     signs = {
@@ -10,62 +10,45 @@ return {
       topdelete = { text = "▔" },
       changedelete = { text = "▌" },
     },
-    signs_staged = {
-      add = { text = "" },
-      change = { text = "" },
-      delete = { text = "" },
-      topdelete = { text = "" },
-      changedelete = { text = "" },
-      untracked = { text = "" },
-    },
-    signs_staged_enable = true,
     signcolumn = false,
     numhl = true,
     on_attach = function(bufnr)
       local gs = require "gitsigns"
 
-      local function map(mode, l, r, opts)
-        opts = opts or {}
-        opts.buffer = bufnr
-        vim.keymap.set(mode, l, r, opts)
-      end
+      local opts = { buffer = bufnr }
 
       -- Navigation
       map("n", "]c", function()
         if vim.wo.diff then
           vim.cmd.normal { "]c", bang = true }
-        elseif string.sub(vim.fn.expand "%", 1, 8) == "fugitive" then
-          gs.nav_hunk("next", { target = "all" })
         else
           gs.nav_hunk "next"
         end
-      end)
+      end, opts)
 
       map("n", "[c", function()
         if vim.wo.diff then
           vim.cmd.normal { "[c", bang = true }
-        elseif string.sub(vim.fn.expand "%", 1, 8) == "fugitive" then
-          gs.nav_hunk("prev", { target = "all" })
         else
           gs.nav_hunk "prev"
         end
-      end)
+      end, opts)
 
       -- Actions
-      map("n", "<leader>hs", gs.stage_hunk)
-      map("n", "<leader>hr", gs.reset_hunk)
-      map("v", "<leader>hs", function() gs.stage_hunk { vim.fn.line ".", vim.fn.line "v" } end)
-      map("v", "<leader>hr", function() gs.reset_hunk { vim.fn.line ".", vim.fn.line "v" } end)
-      map("n", "<leader>hS", gs.stage_buffer)
-      map("n", "<leader>hu", gs.undo_stage_hunk)
-      map("n", "<leader>hU", gs.reset_buffer_index)
-      map("n", "<leader>hR", gs.reset_buffer)
-      map("n", "<leader>hp", gs.preview_hunk)
-      map("n", "<leader>hb", function() gs.blame_line { full = true } end)
-      map("n", "<leader>htb", gs.toggle_current_line_blame)
-      map("n", "<leader>hd", gs.diffthis)
-      map("n", "<leader>hD", function() gs.diffthis "~" end)
-      map("n", "<leader>htd", gs.toggle_deleted)
+      map("n", "<leader>hs", gs.stage_hunk, opts)
+      map("n", "<leader>hr", gs.reset_hunk, opts)
+      map("v", "<leader>hs", function() gs.stage_hunk { vim.fn.line ".", vim.fn.line "v" } end, opts)
+      map("v", "<leader>hr", function() gs.reset_hunk { vim.fn.line ".", vim.fn.line "v" } end, opts)
+      map("n", "<leader>hS", gs.stage_buffer, opts)
+      map("n", "<leader>hu", gs.undo_stage_hunk, opts)
+      map("n", "<leader>hU", gs.reset_buffer_index, opts)
+      map("n", "<leader>hR", gs.reset_buffer, opts)
+      map("n", "<leader>hp", gs.preview_hunk, opts)
+      map("n", "<leader>hb", function() gs.blame_line { full = true } end, opts)
+      map("n", "<leader>htb", gs.toggle_current_line_blame, opts)
+      map("n", "<leader>hd", gs.diffthis, opts)
+      map("n", "<leader>hD", function() gs.diffthis "~" end, opts)
+      map("n", "<leader>htd", gs.toggle_deleted, opts)
 
       -- Text object
       map({ "o", "x" }, "ih", "<cmd>Gitsigns select_hunk<CR>")
@@ -73,12 +56,6 @@ return {
   },
   config = function(_, opts)
     require("gitsigns").setup(opts)
-
-    _G.highlight("GitSignsStagedAddNr", { link = "LineNr" })
-    _G.highlight("GitSignsStagedChangeNr", { link = "LineNr" })
-    _G.highlight("GitSignsStagedDeleteNr", { link = "LineNr" })
-    _G.highlight("GitSignsStagedTopdeleteNr", { link = "LineNr" })
-    _G.highlight("GitSignsStagedChangedeleteNr", { link = "LineNr" })
 
     local colors = require("themes").colors().gitsigns
 
