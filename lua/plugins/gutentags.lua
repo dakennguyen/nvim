67d49db6017a8734c610b7fe58b949c95c7e3817
Khoa Nguyen <khoa.nguyen@tum.de>
2025-03-23 11:04:15 +0100

    Update config

--- lua/plugins/gutentags.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "ludovicchabant/vim-gutentags",
-  init = _G.lazy_load "vim-gutentags",
-  config = function()
-    --vim.g.gutentags_cache_dir = get(g:, 'gutentags_cache_dir', expand('~/.cache/tags'))
-    vim.g.gutentags_ctags_exclude =
-      { "*.min.js", "*.min.css", "build", "vendor", ".git", "node_modules", "*.vim/bundle/*" }
-  end,
-}
