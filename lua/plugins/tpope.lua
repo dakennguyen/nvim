0f12ed48b3255bce01959dcf28ceace157cd9aa1
Khoa Nguyen <dakennguyen@gmail.com>
2025-03-31 01:01:34 +0200

    Add vim-rsi

--- lua/plugins/tpope.lua
+++ lua/plugins/tpope.lua
@@ -4,5 +4,6 @@ return {
   { "tpope/vim-bundler", event = "BufWinEnter Gemfile*" },
   { "tpope/vim-repeat", init = _G.lazy_load "vim-repeat" },
   { "tpope/vim-sleuth", lazy = false },
+  { "tpope/vim-rsi", event = { "InsertEnter", "CmdlineEnter" } },
   { "dakennguyen/vim-unimpaired", event = "VeryLazy" },
 }
