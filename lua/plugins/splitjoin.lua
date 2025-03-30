936460f39fb5157f72bf88d60b4269283c778d6f
Khoa Nguyen <dakennguyen@gmail.com>
2025-03-30 11:48:05 +0200

    Use mini.splitjoin instead of treesj

---
+++ lua/plugins/splitjoin.lua
@@ -0,0 +1,12 @@
+return {
+  "echasnovski/mini.splitjoin",
+  version = "*",
+  keys = { "<leader><leader>", "gS", "gJ" },
+  opts = {
+    mappings = {
+      toggle = "<leader><leader>",
+      split = "gS",
+      join = "gJ",
+    },
+  },
+}
