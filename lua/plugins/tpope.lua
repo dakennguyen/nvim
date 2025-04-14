aaf0786a521f114035df2d25ab1ed1d407620606
Khoa Nguyen <khoa.nguyen@check24.de>
2025-04-14 15:20:05 +0200

    Revert "Use mini.surround instead of vim-surround"
    This reverts commit 988fe13fabf0203a7ffe4d0f713649433f2f684c.

--- lua/plugins/tpope.lua
+++ lua/plugins/tpope.lua
@@ -1,5 +1,6 @@
 return {
   { "tommcdo/vim-fubitive", event = "VeryLazy" },
+  { "tpope/vim-surround", init = _G.lazy_load "vim-surround" },
   { "tpope/vim-repeat", init = _G.lazy_load "vim-repeat" },
   { "tpope/vim-sleuth", lazy = false },
   { "tpope/vim-rsi", event = { "InsertEnter", "CmdlineEnter" } },
