a33b580810877385cf39ba333c92d3be220875ad
Khoa Nguyen <dakennguyen@gmail.com>
2025-12-25 12:42:20 +0100

    Update treesitter to main

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -20,9 +20,10 @@ _G.augroup("my.lsp", {
   event = "LspAttach",
   callback = function(args)
     local client = vim.lsp.get_client_by_id(args.data.client_id)
-    if client:supports_method "textDocument/foldingRange" then
+    if client and client:supports_method "textDocument/foldingRange" then
       local win = vim.api.nvim_get_current_win()
       vim.wo[win][0].foldexpr = "v:lua.vim.lsp.foldexpr()"
+      vim.wo[win][0].foldmethod = "expr"
       vim.wo[win][0].foldtext = ""
     end
 
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,83 +1,77 @@
 vim.pack.add {
   "https://github.com/nvim-treesitter/nvim-treesitter",
-  "https://github.com/nvim-treesitter/nvim-treesitter-textobjects",
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter-textobjects", version = "main" },
   "https://github.com/Wansmer/treesj",
   "https://github.com/windwp/nvim-ts-autotag",
 }
 
-require("nvim-treesitter.configs").setup {
-  auto_install = true,
-  ignore_install = { "tmux" },
-  highlight = {
-    enable = true,
-  },
-  indent = {
-    enable = true,
-    disable = { "ruby", "go" },
-  },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = "<cr>",
-      node_incremental = "<cr>",
-      scope_incremental = false,
-      node_decremental = "<bs>",
-    },
-  },
-  -- nvim-treesitter-textobjects
-  textobjects = {
-    select = {
-      enable = true,
-      lookahead = true,
-      keymaps = {
-        ["am"] = "@function.outer",
-        ["im"] = "@function.inner",
-        ["ac"] = "@class.outer",
-        ["ic"] = "@class.inner",
-      },
-    },
-    move = {
-      enable = true,
-      set_jumps = true,
-      goto_next_start = {
-        ["]m"] = "@function.outer",
-        ["]]"] = "@class.outer",
-      },
-      goto_next_end = {
-        ["]M"] = "@function.outer",
-        ["]["] = "@class.outer",
-      },
-      goto_previous_start = {
-        ["[m"] = "@function.outer",
-        ["[["] = "@class.outer",
-      },
-      goto_previous_end = {
-        ["[M"] = "@function.outer",
-        ["[]"] = "@class.outer",
-      },
-    },
-    swap = {
-      enable = false,
-      swap_next = {
-        ["<leader>a"] = "@parameter.inner",
-      },
-      swap_previous = {
-        ["<leader>A"] = "@parameter.inner",
-      },
-    },
-    lsp_interop = {
-      enable = false,
-      border = "single",
-      peek_definition_code = {
-        ["<leader>df"] = "@function.outer",
-        ["<leader>dF"] = "@class.outer",
-      },
-    },
-  },
-}
+-- ##########################
+-- nvim-treesitter
+-- ##########################
+-- npm install -g tree-sitter-cli
+_G.augroup("TreesitterSetup", {
+  event = "FileType",
+  callback = function(event)
+    -- Skip treesitter on large files
+    -- local stats = vim.uv.fs_stat(vim.api.nvim_buf_get_name(event.buf))
+    -- if vim.api.nvim_buf_line_count(event.buf) > 5000 or (stats and stats.size > 100 * 1024) then return end
 
-vim.o.foldmethod = "expr"
-vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+    if not vim.api.nvim_buf_is_valid(event.buf) then return end
+
+    local lang = vim.treesitter.language.get_lang(event.match) or event.match
+    local ok_parsers, parser_configs = pcall(require, "nvim-treesitter.parsers")
+
+    if lang ~= "kulala_http" then
+      if not ok_parsers or not parser_configs[lang] then return end
+      require("nvim-treesitter").install { lang }
+    end
+
+    local ok = pcall(vim.treesitter.start, event.buf, lang)
+    if ok then
+      vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+      vim.wo[0][0].foldmethod = "expr"
+    end
+  end,
+})
+
+-- ##########################
+-- nvim-treesitter-textobjects
+-- ##########################
+local select = require "nvim-treesitter-textobjects.select"
+vim.keymap.set({ "x", "o" }, "am", function() select.select_textobject("@function.outer", "textobjects") end)
+vim.keymap.set({ "x", "o" }, "im", function() select.select_textobject("@function.inner", "textobjects") end)
+vim.keymap.set({ "x", "o" }, "ac", function() select.select_textobject("@class.outer", "textobjects") end)
+vim.keymap.set({ "x", "o" }, "ic", function() select.select_textobject("@class.inner", "textobjects") end)
+vim.keymap.set({ "x", "o" }, "as", function() select.select_textobject("@local.scope", "locals") end)
+
+local move = require "nvim-treesitter-textobjects.move"
+local mode = { "n", "x", "o" }
+vim.keymap.set(mode, "]m", function() move.goto_next_start("@function.outer", "textobjects") end)
+vim.keymap.set(mode, "]]", function() move.goto_next_start("@class.outer", "textobjects") end)
+vim.keymap.set(mode, "]o", function() move.goto_next_start({ "@loop.inner", "@loop.outer" }, "textobjects") end)
+vim.keymap.set(mode, "]s", function() move.goto_next_start("@local.scope", "locals") end)
+vim.keymap.set(mode, "]z", function() move.goto_next_start("@fold", "folds") end)
+
+vim.keymap.set(mode, "]M", function() move.goto_next_end("@function.outer", "textobjects") end)
+vim.keymap.set(mode, "][", function() move.goto_next_end("@class.outer", "textobjects") end)
+
+vim.keymap.set(mode, "[m", function() move.goto_previous_start("@function.outer", "textobjects") end)
+vim.keymap.set(mode, "[[", function() move.goto_previous_start("@class.outer", "textobjects") end)
+
+vim.keymap.set(mode, "[M", function() move.goto_previous_end("@function.outer", "textobjects") end)
+vim.keymap.set(mode, "[]", function() move.goto_previous_end("@class.outer", "textobjects") end)
+
+-- vim.keymap.set(mode, "]d", function() move.goto_next("@conditional.outer", "textobjects") end)
+-- vim.keymap.set(mode, "[d", function() move.goto_previous("@conditional.outer", "textobjects") end)
+
+local ts_repeat_move = require "nvim-treesitter-textobjects.repeatable_move"
+vim.keymap.set(mode, ";", ts_repeat_move.repeat_last_move)
+vim.keymap.set(mode, ",", ts_repeat_move.repeat_last_move_opposite)
+vim.keymap.set(mode, "f", ts_repeat_move.builtin_f_expr, { expr = true })
+vim.keymap.set(mode, "F", ts_repeat_move.builtin_F_expr, { expr = true })
+vim.keymap.set(mode, "t", ts_repeat_move.builtin_t_expr, { expr = true })
+vim.keymap.set(mode, "T", ts_repeat_move.builtin_T_expr, { expr = true })
 
 -- ##########################
 -- treesj
