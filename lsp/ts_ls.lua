bf693b69e8ac194cff2769e2a051ad72b26dd965
Khoa Nguyen <dakennguyen@gmail.com>
2025-12-25 12:58:14 +0100

    Resolve warnings

--- lsp/ts_ls.lua
+++ lsp/ts_ls.lua
@@ -2,7 +2,7 @@
 return {
   cmd = { "typescript-language-server", "--stdio" },
   root_markers = { "tsconfig.json", "jsconfig.json", "package.json", ".git" },
-  filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
+  filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
   init_options = {
     hostInfo = "neovim",
   },
