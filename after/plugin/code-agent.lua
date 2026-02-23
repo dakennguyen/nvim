d5fed209bc1c8814d45d86b96f4041e5259f1558
Khoa Nguyen <khoa.nguyen@check24.de>
2026-02-23 10:01:38 +0100

    Add `Agent` command

--- after/plugin/code-agent.lua
+++ after/plugin/code-agent.lua
@@ -15,15 +15,26 @@ local function show_terminal()
   return true
 end
 
-local function create_terminal()
+local function create_terminal(command)
   vim.cmd "vert botright 100 new"
   code_agent_win = vim.api.nvim_get_current_win()
-  vim.cmd "terminal $MYCODEAGENT"
+  vim.cmd("terminal " .. (command or "$MYCODEAGENT"))
   code_agent_buf = vim.api.nvim_get_current_buf()
 end
 
 local function toggle_code_agent()
   local _ = focus_terminal() or show_terminal() or create_terminal()
+  vim.cmd "startinsert"
+end
+
+local function run_code_agent(args)
+  local command = "$MYCODEAGENT"
+  if args and #args > 0 then
+    local escaped_args = vim.tbl_map(vim.fn.shellescape, args)
+    command = command .. " " .. table.concat(escaped_args, " ")
+  end
+
+  create_terminal(command)
   vim.cmd "startinsert"
 end
 
@@ -53,6 +64,18 @@ local function paste_file_code_agent()
   vim.api.nvim_put({ pos }, "c", true, true)
 end
 
-vim.keymap.set("n", "<leader>a", toggle_code_agent, { silent = true })
+vim.api.nvim_create_user_command("Agent", function(opts)
+  if #opts.fargs == 0 then
+    toggle_code_agent()
+    return
+  end
+
+  run_code_agent(opts.fargs)
+end, {
+  nargs = "*",
+  desc = "Open or focus Code Agent terminal (accepts CLI args)",
+})
+
+vim.keymap.set("n", "<leader>a", "<cmd>Agent<CR>", { silent = true })
 vim.keymap.set("v", "<leader>a", paste_range_code_agent, { silent = true })
 vim.keymap.set("n", "<leader>A", paste_file_code_agent, { silent = true })
