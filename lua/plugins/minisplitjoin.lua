6b5eba6849732b19b4a84ac6eb26c8e7bf2ac104
Khoa Nguyen <khoa.nguyen@check24.de>
2026-04-15 10:11:19 +0200

    Use mini.splitjoin instead of treesj

---
+++ lua/plugins/minisplitjoin.lua
@@ -0,0 +1,17 @@
+vim.pack.add { "https://github.com/nvim-mini/mini.splitjoin" }
+
+local gen_hook = require("mini.splitjoin").gen_hook
+local curly = { brackets = { "%b{}" } }
+
+local del_comma_curly = gen_hook.del_trailing_separator(curly)
+local pad_curly = gen_hook.pad_brackets(curly)
+
+require("mini.splitjoin").setup {
+  mappings = {
+    split = "]j",
+    join = "[j",
+  },
+  join = {
+    hooks_post = { del_comma_curly, pad_curly },
+  },
+}
